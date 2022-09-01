19e8308090297fce639679ab8b29094dfa29cf68
mon <mon@mon.com>
2022-09-01 11:52:38 +0800

    commit

--- config/app.php
+++ config/app.php
@@ -9,21 +9,14 @@
 
 return [
     // 是否调试模式
-    'debug'             => true,
+    'debug'     => true,
     // 时区
-    'timezone'          => 'PRC',
-    // 存储主进程PID的文件
-    'pid_file'          => RUNTIME_PATH . '/gaia.pid',
-    // 存储主进程状态文件的文件
-    'status_file'       => RUNTIME_PATH . '/gaia.status',
-    // 存储标准输出的文件
-    'stdout_file'       => RUNTIME_PATH . '/stdout.log',
-    // 存储日志文件
-    'log_file'          => RUNTIME_PATH . '/workerman.log',
-    // 默认的最大可接受数据包大小
-    'max_package_size'  => 10 * 1024 * 1024,
-    // workerman事件循环使用对象，默认 \Workerman\Events\Select
-    'event_loop'        => '',
-    // 发送停止命令后，多少秒内程序没有停止，则强制停止
-    'stop_timeout'      => 2
+    'timezone'  => 'PRC',
+    // 监控服务
+    'monitor'   => [
+        // 监控的文件目录
+        'paths' => [APP_PATH, CONFIG_PATH],
+        // 监控指定后缀名文件
+        'exts'  => ['php', 'html'],
+    ]
 ];
