879e54e70be333a44e5a379dc04534314e4b095c
mon <mon@mon.com>
2022-09-27 16:12:36 +0800

    commit

--- config/app.php
+++ config/app.php
@@ -12,6 +12,21 @@ return [
     'debug'     => true,
     // 时区
     'timezone'  => 'PRC',
+    // worker进程配置
+    'worker'    => [
+        // 默认的最大可接受数据包大小
+        'max_package_size'  => 10 * 1024 * 1024,
+        // 存储主进程PID的文件
+        'pid_file'          => RUNTIME_PATH . '/gaia.pid',
+        // workerman日志记录文件
+        'log_file'          => RUNTIME_PATH . '/workerman.log',
+        // 存储主进程状态信息的文件，运行 status 指令后，内容会写入该文件
+        'status_file'       => RUNTIME_PATH . '/gaia.status',
+        // workerman事件循环使用对象，默认 \Workerman\Events\Select。一般不需要修改，空则可以
+        'event_loop_class'  => '',
+        // 发送停止命令后，多少秒内程序没有停止，则强制停止
+        'stop_timeout'      => 2
+    ],
     // 监控服务
     'monitor'   => [
         // 监控的文件目录
--- support/bootstrap.php
+++ support/bootstrap.php
@@ -1,7 +1,5 @@
 <?php
 
-use Workerman\Worker;
-use Workerman\Connection\TcpConnection;
 
 /*
 |--------------------------------------------------------------------------
@@ -65,52 +63,3 @@ define('CONFIG_PATH', ROOT_PATH . '/config');
 define('RUNTIME_PATH', ROOT_PATH . '/runtime');
 
 
-/*
-|--------------------------------------------------------------------------
-| 接管错误处理
-|--------------------------------------------------------------------------
-| 这里定义PHP错误及程序结束处理方法
-|
-*/
-set_error_handler(function ($level, $message, $file = '', $line = 0, $context = []) {
-    if (error_reporting() & $level) {
-        // 所有错误抛出异常
-        throw new ErrorException($message, 0, $level, $file, $line);
-    }
-});
-register_shutdown_function(function ($start_time) {
-    // 进程平滑开关
-    if (Worker::getAllWorkers()) {
-        if (time() - $start_time <= 1) {
-            sleep(1);
-        }
-    }
-}, time());
-
-
-
-// 默认的最大可接受数据包大小
-TcpConnection::$defaultMaxPackageSize = 10 * 1024 * 1024;
-// 存储主进程PID的文件
-Worker::$pidFile = RUNTIME_PATH . '/gaia.pid';
-// 存储标准输出的文件，默认 /dev/null。daemonize运行模式下echo的内容才会记录到文件中
-Worker::$stdoutFile = RUNTIME_PATH . '/stdout.log';
-// workerman日志记录文件
-Worker::$logFile = RUNTIME_PATH . '/workerman.log';
-// 存储主进程状态信息的文件，运行 status 指令后，内容会写入该文件
-Worker::$statusFile = RUNTIME_PATH . '/gaia.status';
-// workerman事件循环使用对象，默认 \Workerman\Events\Select。一般不需要修改，空则可以
-Worker::$eventLoopClass = '';
-// 发送停止命令后，多少秒内程序没有停止，则强制停止
-Worker::$stopTimeout = 2;
-// 重置opcache缓存
-Worker::$onMasterReload = function () {
-    if (function_exists('opcache_get_status')) {
-        $status = opcache_get_status();
-        if ($status && isset($status['scripts'])) {
-            foreach (array_keys($status['scripts']) as $file) {
-                opcache_invalidate($file, true);
-            }
-        }
-    }
-};
