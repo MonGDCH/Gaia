36d8d903829f6417fe4bd9ada03f2cc5c8d6638e
monlam <985558837@qq.com>
2023-05-03 16:56:43 +0800

    更新依赖

--- support/helpers.php
+++ support/helpers.php
@@ -6,3 +6,17 @@
 | 定义公用的全局函数
 |
 */
+
+if (!function_exists('env')) {
+    /**
+     * 获取环境变量配置信息
+     *
+     * @param string $key   配置名
+     * @param mixed $default    默认值
+     * @return mixed
+     */
+    function env(string $key, $default = null)
+    {
+        return \mon\env\Env::get($key, $default);
+    }
+}
