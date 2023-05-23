87ef8a9da8e8db7a7e9e4d95dcdf837a652d3302
monlam <985558837@qq.com>
2023-05-23 15:00:25 +0800

    commit

--- config/log.php
+++ config/log.php
@@ -9,7 +9,6 @@
 
 use mon\log\format\LineFormat;
 use mon\log\record\FileRecord;
-use mon\log\record\FileBatchRecord;
 
 return [
     // 通道
@@ -19,7 +18,18 @@ return [
             // 类名
             'handler'   => LineFormat::class,
             // 配置信息
-            'config'    => []
+            'config'    => [
+                // 日志是否包含级别
+                'level'         => true,
+                // 日志是否包含时间
+                'date'          => true,
+                // 时间格式，启用日志时间时有效
+                'date_format'   => 'Y-m-d H:i:s',
+                // 是否启用日志追踪
+                'trace'         => false,
+                // 追踪层级，启用日志追踪时有效
+                'layer'         => 3
+            ]
         ],
         // 记录器
         'record'    => [
@@ -27,41 +37,19 @@ return [
             'handler'   => FileRecord::class,
             // 配置信息
             'config'    => [
+                // 是否自动写入文件
+                'save'      => true,
+                // 写入文件后，清除缓存日志
+                'clear'     => true,
+                // 日志名称，空则使用当前日期作为名称       
+                'logName'   => '',
                 // 日志文件大小
                 'maxSize'   => 20480000,
                 // 日志目录
                 'logPath'   => RUNTIME_PATH . '/log',
                 // 日志滚动卷数   
-                'rollNum'   => 3,
-                // 日志名称，空则使用当前日期作为名称       
-                'logName'   => '',
+                'rollNum'   => 3
             ]
         ]
-    ],
-    // http日志通道
-    'http' => [
-        // 记录器
-        'record'    => [
-            // 类名
-            'handler'   => FileBatchRecord::class,
-            // 配置信息
-            'config'    => [
-                // 日志目录
-                'logPath'   => RUNTIME_PATH . '/log/http',
-            ]
-        ]
-    ],
-    // fpm日志通道
-    'fpm' => [
-        // 记录器
-        'record'    => [
-            // 类名
-            'handler'   => FileBatchRecord::class,
-            // 配置信息
-            'config'    => [
-                // 日志目录
-                'logPath'   => RUNTIME_PATH . '/log/fpm',
-            ]
-        ]
-    ],
+    ]
 ];
